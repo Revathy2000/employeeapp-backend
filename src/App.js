f648a9f73540e56caeb0c5cf5f87f2f547b42770
Revathy2000 <revathyr1914@gmail.com>
2024-01-08 16:05:58 +0000

    backend first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import Create from './components/create/Create';
+import Read from './components/read/Read';
+import {BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import image from "./bgimg1.jpeg"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='main' style={{ backgroundImage:`url(${image})`,backgroundRepeat:"no-repeat",backgroundSize:"cover", 
+    height:700,width:1500}}>
+        <div id='title'>
+          <h2>Employee Dashboard</h2>
+        </div>
+        <div class='buttons'> 
+          <BrowserRouter>
+          <div class='sty'>
+            <Link to='/'>
+            <button class="btn btn-outline-dark me-2" type="button">Home</button>
+            </Link>
+            <Link to='/Create'>
+            <button class="btn btn-sm btn-outline-secondary" type="button">Employee form</button>
+            </Link>
+            </div>
+            <Routes>
+              <Route path='/Create' element={<Create />} />
+              <Route path='/' element={<Read />} />
+            </Routes>
+          </BrowserRouter>
+          </div>
+      </div>
   );
 }
 
